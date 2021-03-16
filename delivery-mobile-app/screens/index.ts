4df25b48d2276b74a469b2b1f5121f7db2dd369f
jperezr-psl <juan.perezr@perficient.com>
2021-03-16 17:16:32 -0500

    location screen

--- delivery-mobile-app/screens/index.ts
+++ delivery-mobile-app/screens/index.ts
@@ -2,4 +2,4 @@ export { default as LocationScreen } from "./LocationScreen";
 export { default as NotFoundScreen } from "./NotFoundScreen";
 export { default as TabCartScreen } from "./TabCartScreen";
 export { default as TabMainScreen } from "./TabMainScreen";
-export { default as TabSearchScreen } from "./TabSearchScreen";
+export { default as TabSearchScreen } from "./TabSearchScreen"; 
