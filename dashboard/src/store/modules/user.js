edad6a4d1e3d6b44c7117aa4ee22b143016cde3d
Edgar Jimenez <edgar.jimenezg@perficient.com>
2021-04-17 16:58:12 -0500

    delete user log in the dashboard

--- dashboard/src/store/modules/user.js
+++ dashboard/src/store/modules/user.js
@@ -30,7 +30,6 @@ const mutations = {
 
 const actions = {
   authInLogin({ state }) {
-    console.log('se llamo authinlogin')
     this._vm.$socket.client.emit('authorization', state.token)
   },
 
