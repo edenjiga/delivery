88332f7567d98b2cb2b821feb3f6383587495b86
Edgar Jimenez <edgar.jimenezg@perficient.com>
2021-04-22 15:17:55 -0500

    testing socket service

--- socket-api/src/app.module.ts
+++ socket-api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { NatsListenersModule } from './NatsListeners';
 import { SocketSubscribersModule } from './socketSubcribers/socketSubcribers.module';
 
 @Module({
   imports: [NatsListenersModule, SocketSubscribersModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
