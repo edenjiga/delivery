bc00ff3f9a400b6c4e6ff1eca7bcbb7b313825cd
Edgar Jimenez <edgar.jimenezg@perficient.com>
2021-04-23 12:49:31 -0500

    test socket service

--- socket-api/src/socketSubcribers/authorization.gateway.ts
+++ socket-api/src/socketSubcribers/authorization.gateway.ts
@@ -2,6 +2,7 @@ import { Logger } from '@nestjs/common';
 
 import {
   OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -10,19 +11,24 @@ import { Server, Socket } from 'socket.io';
 import { SocketHandlerEventsUseCases } from 'src/useCases';
 
 @WebSocketGateway()
-export class AuthorizationGateway implements OnGatewayConnection {
+export class AuthorizationGateway
+  implements OnGatewayConnection, OnGatewayDisconnect {
   private logger: Logger = new Logger('AppGateway');
 
   @WebSocketServer()
   public server: Server;
 
   constructor(private socketHandleEvent: SocketHandlerEventsUseCases) {}
+  handleDisconnect(client: any) {
+    this.logger.log('Client disconnect');
+  }
   handleConnection(client: any, ...args: any[]) {
     this.logger.log('Client connect');
   }
 
   @SubscribeMessage('authorization')
   handleEvent(client: Socket, token: string) {
+    this.logger.log('authorization is call');
     this.socketHandleEvent.handleAuthizationSocketRequest(client, token);
   }
 }
