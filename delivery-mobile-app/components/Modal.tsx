782be7a0f25ea7e5020455cac9468e26096bf904
Edgar Jimenez <edgar.jimenezg@perficient.com>
2021-05-12 21:09:14 -0500

    add margin modal

--- delivery-mobile-app/components/Modal.tsx
+++ delivery-mobile-app/components/Modal.tsx
@@ -9,7 +9,7 @@ import { Text, View } from '@/components/Themed';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/store';
 import { IModalState } from '@/types';
-import { setModalState } from '@/store/actions/modal';
+import { setModalIsVisibleState, setModalState } from '@/store/actions/modal';
 import Colors from '@/constants/Colors';
 
 const Modal: FC = () => {
@@ -20,7 +20,7 @@ const Modal: FC = () => {
 
   const setIsVisible = useCallback(
     (value: boolean) => {
-      dispatch(setModalState({ isVisible: value }));
+      dispatch(setModalIsVisibleState(value));
     },
     [dispatch],
   );
@@ -36,21 +36,8 @@ const Modal: FC = () => {
               source={require('assets/images/info.png')}
             />
           </View>
-          <View style={styles.closeWrapper}>
-            <TouchableOpacity
-              onPress={() => setIsVisible(false)}
-              style={styles.imageClose}
-            >
-              <Image
-                style={styles.closeIcon}
-                resizeMode="contain"
-                source={require('assets/images/orangeClose.png')}
-              />
-            </TouchableOpacity>
-          </View>
 
-          <Text style={styles.textInfo}>¿seguro que Deseas cerrar sesión?</Text>
-          <Text style={styles.textDescription}>No se aplicarán cargos</Text>
+          <Text style={styles.textInfo}>{modalText}</Text>
           <TouchableOpacity
             style={styles.close}
             onPress={() => setIsVisible(false)}
@@ -64,76 +51,73 @@ const Modal: FC = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.8)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  header: {
-    backgroundColor: Colors.orange,
-    width: '100%',
-    borderTopStartRadius: 10,
-    borderTopRightRadius: 10,
-    paddingVertical: 10,
-    marginBottom: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   centeredView: {
-    backgroundColor: Colors.white,
-    width: '70%',
-    borderRadius: 20,
-    justifyContent: 'center',
     alignItems: 'center',
-    paddingBottom: 20,
-
-    shadowOffset: { width: 0, height: 0 },
-    shadowColor: Colors.lightgrey,
-    shadowOpacity: 0.6,
+    backgroundColor: Colors.white,
+    borderRadius: 20,
     elevation: 2,
-  },
-  textInfo: {
+    justifyContent: 'center',
+    paddingBottom: 20,
+    shadowColor: Colors.lightgrey,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.6,
     width: '70%',
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    lineHeight: 20,
-  },
-  textDescription: {
-    width: '70%',
-    textAlign: 'center',
-    marginTop: 5,
   },
   close: {
-    marginTop: 15,
+    alignItems: 'center',
     backgroundColor: Colors.orange,
+    borderRadius: 6,
+    justifyContent: 'center',
+    marginTop: 15,
     minHeight: 36,
     minWidth: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 6,
+  },
+  closeIcon: {
+    height: 26,
+    width: 26,
   },
   closeText: {
     color: Colors.white,
-    fontWeight: 'bold',
     fontSize: 17,
+    fontWeight: 'bold',
   },
   closeWrapper: {
-    width: '100%',
-    justifyContent: 'center',
     alignItems: 'flex-end',
+    justifyContent: 'center',
     marginBottom: 10,
+    width: '100%',
   },
-  imageClose: {
-    marginHorizontal: 20,
+  container: {
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  header: {
+    alignItems: 'center',
+    backgroundColor: Colors.orange,
+    borderTopRightRadius: 10,
+    borderTopStartRadius: 10,
+    justifyContent: 'center',
+    marginBottom: 10,
+    paddingVertical: 10,
+    width: '100%',
   },
   infoIcon: {
-    width: 40,
     height: 40,
+    width: 40,
   },
-  closeIcon: {
-    width: 26,
-    height: 26,
+  textDescription: {
+    marginTop: 5,
+    textAlign: 'center',
+    width: '70%',
+  },
+  textInfo: {
+    lineHeight: 20,
+    marginBottom: 15,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    width: '70%',
   },
 });
 export default Modal;
